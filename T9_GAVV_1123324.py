44fc8f1838a584fb9b2d89a10a753220aebc73d1
w5mp1 <158320914+w5mp1@users.noreply.github.com>
2024-05-15 10:52:36 -0600

    Add files via upload

---
+++ T9_GAVV_1123324.py
@@ -0,0 +1,16 @@
+print("Genesis Vides - 1123324")
+n1=int(input("Ingrese un número:"))
+print()
+print("Resultado:")
+
+# operaciones a realizar
+
+milla= (n1/1609.34)
+kilometro= int(n1/1000)
+pie=int(n1*3.28)
+pulgada=int(n1*39.3701)
+
+print("Millas=", milla)
+print("Kilometros=", kilometro)
+print("Pies=", pie)
+print("Pulgadas=",pulgada)
